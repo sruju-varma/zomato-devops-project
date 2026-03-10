30306d2eeb7eccf3fde89b1f4050c5896712a932
srujana varma <sruju999@gmail.com>
2026-03-10 20:16:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
 const http = require("http");
 
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>Zomato DevOps</title>
+<style>
+body{
+font-family: Arial;
+background:#f8f8f8;
+text-align:center;
+}
+.header{
+background:#e23744;
+color:white;
+padding:20px;
+font-size:28px;
+}
+.card{
+background:white;
+margin:20px auto;
+padding:20px;
+width:300px;
+border-radius:10px;
+box-shadow:0 2px 10px rgba(0,0,0,0.1);
+}
+</style>
+</head>
+
+<body>
+
+<div class="header">
+Zomato DevOps Project
+</div>
+
+<div class="card">
+<h2>Pizza Hub</h2>
+<p>Italian Pizza Restaurant</p>
+</div>
+
+<div class="card">
+<h2>Burger King</h2>
+<p>Best Burgers in Town</p>
+</div>
+
+<div class="card">
+<h2>Indian Spice</h2>
+<p>Authentic Indian Food</p>
+</div>
+
+</body>
+</html>
+`;
+
 const server = http.createServer((req,res)=>{
-res.write("Zomato DevOps App Running Successfully");
+res.write(html);
 res.end();
 });
 
